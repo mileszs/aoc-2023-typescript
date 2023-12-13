782faa5eb873b2093c2cfbc6853280884967b5b4
Miles Z. Sterrett <miles@mileszs.com>
2023-12-13 08:26:42 -0500

    Day 1a solution

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -3,8 +3,14 @@ import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const sum = data.map((x) => {
+    const allDigits = x.match(/\d/g).join('');
+    return allDigits[0] + allDigits.at(-1);
+  }).reduce((acc, x) => acc + parseInt(x), 0);
+
+  return sum;
 }
 
-const answer = await day1a();
+const answer = await day1a('puzzles/day-1/day-1-a.data.txt');
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
